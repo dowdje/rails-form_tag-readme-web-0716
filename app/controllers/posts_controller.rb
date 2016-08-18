8878982013c132c7dd893c52cee59f43d8b374eb
dowdje <dowdje89@gmail.com>
2016-08-17 20:23:01 -0400

    Finished Form Tag, with no clue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,18 @@
 class PostsController < ApplicationController
 
+  def new
+    byebug
+  end
+
+  def index
+  end
+
+  def create
+    @post = params
+    redirect_to new_post_path(post: @post)
+  end
+
+  def show
+  end
+
 end
